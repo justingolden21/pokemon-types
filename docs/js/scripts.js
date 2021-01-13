6893fbe1e9b646ce982313a745e5be03762875a6
Justin Golden <justingolden21@gmail.com>
2021-01-12 20:31:06 -0800

    push to production

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -4,40 +4,107 @@ if('serviceWorker' in navigator) {
 		.catch(err => console.log('service worker not registered', err));
 }
 
+window.onload = ()=> {
+	document.getElementById('clear-btn').onclick = clearTypes;
+}
+
 const capitalize = (str)=> str.charAt(0).toUpperCase() + str.substring(1);
+
+let typeJson;
+fetch('data/types.json')
+	.then(response => response.json())
+	.then(json => {
+		typeJson = json;
+		for(let type of json) {
+			let btn = document.createElement('button');
+			btn.innerText = capitalize(type.name);
+			btn.classList = `btn ${type.name}`;
+			document.getElementById('type-btns').appendChild(btn);
+			btn.onclick = ()=> {
+				handleClick(btn.classList[1]);
+			};
+		}
+	});
+
+let currentType1, currentType2;
+let recentlyChangedType;
+
+function updateTypeDisplay() {
+	let weakHtml = '';
+	let resistHtml = '';
+	for(let type of typeJson) {
+		let matchup = getMatchup(type.name, currentType1, currentType2);
+		if(matchup == 1) continue;
+		const html = `<div class="matchup-item" style="background-color: #${type.color};">${matchup}x ${type.name}</div>`;
+		if(matchup > 1) {
+			weakHtml += html;
+		} else {
+			resistHtml += html;
+		}
+	}
+	document.getElementById('type-weak').innerHTML = weakHtml;
+	document.getElementById('type-resist').innerHTML = resistHtml;
+}
+
+function handleClick(type) {
+	if(currentType1 == type) {
+		currentType1 = '';
+		removeActive(type);
+	} else if (currentType2 == type) {
+		currentType2 = '';
+		removeActive(type);
+	} else {
+		if(!currentType1) {
+			changeType(1, type);
+		} else if(!currentType2) {
+			changeType(2, type);
+		} else {
+			if(recentlyChangedType == 1) {
+				removeActive(currentType2);
+				changeType(2, type);
+			} else {
+				removeActive(currentType1);
+				changeType(1, type);
+			}
+		}
+	}
+
+	updateTypeDisplay();
+}
+
+function changeType(num, type) {
+	if(num == 1) {
+		currentType1 = type;
+	} else {
+		currentType2 = type;
+	}
+	recentlyChangedType = num;
+
+	addActive(type);
+}
+
+function addActive(type) {
+	document.querySelector(`.${type}`).classList.add('active');
+}
+function removeActive(type) {
+	document.querySelector(`.${type}`).classList.remove('active');
+}
+
+function clearTypes() {
+	if(currentType2 != '') {
+		removeActive(currentType2);
+		currentType2 = '';
+	}
+	if(currentType1 != '') {
+		removeActive(currentType1);
+		currentType1 = '';
+	}
+
+	updateTypeDisplay();
+}
 
 // fetch('../data/pokedex.json')
 // 	.then(response => response.json())
 // 	.then(json => {
 // 		console.log(json);
 // 	});
-
-fetch('data/types.json')
-	.then(response => response.json())
-	.then(json => {
-		// console.log(json);
-		for(type of json) {
-			// console.log(type);
-			// console.log(type.name);
-			// console.log(type.color);
-
-
-	// 		console.log(`
-	// .btn.${type.name} {
-	// 	border: 2px solid #${type.color};
-	// 	color: #000;
-	// }
-	// .btn.${type.name}:hover {
-	// 	background-color: #${type.color};
-	// 	color: #FFF;
-	// }
-	// .btn.${type.name}:focus {
-	// 	--tw-ring-shadow: 0 0 0 2px #${type.color}44;
-	// }`);
-
-			let btn = document.createElement('button');
-			btn.innerText = capitalize(type.name);
-			btn.classList = `btn mx-2 my-1 ${type.name}`;
-			document.getElementById('type-btns').appendChild(btn);
-		}
-	});
