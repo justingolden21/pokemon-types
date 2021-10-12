6809f331453fae5de7b8de4e9d9abec9ffb79443
RGB Studios <justingolden21@gmail.com>
2021-10-12 12:28:55 -0700

    store settings to local storage

--- docs/js/settings.js
+++ docs/js/settings.js
@@ -12,20 +12,72 @@ window.addEventListener('load', () => {
 
 		if (checked) updateWeatherBoostDisplay();
 		updateTypeDisplay();
+
+		storeSettingToggle('weather-boost-toggle');
 	};
 
 	document.getElementById('weather-boost-select').onchange = () => {
 		updateWeatherBoostDisplay();
 		updateTypeDisplay();
+
+		// not storing selected weather boost or current pokemon or types for now
 	};
 
 	document.getElementById('use-weather-boost-toggle').onchange = () => {
 		updateTypeDisplay();
+
+		storeSettingToggle('use-weather-boost-toggle');
 	};
 
 	document.getElementById('dark-mode-toggle').onchange = () => {
 		const checked = document.getElementById('dark-mode-toggle').checked;
 		const display = checked ? 'dark' : 'light';
 		document.body.classList.toggle('dark-mode');
+
+		storeSettingToggle('dark-mode-toggle');
 	};
+
+	document.getElementById('clear-weather-toggle').onchange = () => {
+		const checked = document.getElementById('clear-weather-toggle').checked;
+
+		storeSettingToggle('clear-weather-toggle');
+	};
+
+	document.getElementById('type-display-options').onclick = () => {
+		storeSetting('type-display-options', selectVal('type-display-options'));
+		console.log('type-display-options', selectVal('type-display-options'));
+	};
+
+	// read local storage
+	loadSettings();
 });
+
+function loadSettings() {
+	const toggles = [
+		'weather-boost',
+		'use-weather-boost',
+		'dark-mode',
+		'clear-weather',
+	];
+	for (let toggle of toggles) {
+		const name = toggle + '-toggle';
+		const val = localStorage.getItem(name) === 'true';
+		const elm = document.getElementById(name);
+
+		if (elm.checked == val) {
+			elm.onchange();
+		}
+		elm.checked = val;
+		elm.onchange();
+	}
+}
+
+function storeSettingToggle(name) {
+	const key = name;
+	const val = document.getElementById(name).checked;
+	storeSetting(key, val);
+}
+
+function storeSetting(key, val) {
+	localStorage.setItem(key, val);
+}
