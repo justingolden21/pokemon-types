b26e967af12d8f03dbe8497d741dbbde2e303165
Justin Golden <justingolden21@gmail.com>
2022-06-14 22:29:59 -0700

    Move google analytics to app.html and remove redundant body tag

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -301,13 +301,51 @@
 	}
 </script>
 
-<body class="text-center m-4 text-gray-900">
-	<section>
-		<button
-			class="btn hover-fill modal-btn float-left ml-2"
-			on:click={() => open('settingsModal')}
-			name="Settings"
+<section class="text-center m-4 text-gray-900">
+	<button
+		class="btn hover-fill modal-btn float-left ml-2"
+		on:click={() => open('settingsModal')}
+		name="Settings"
+	>
+		<svg
+			class="w-6 h-6"
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
 		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+			/>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+			/>
+		</svg>
+	</button>
+	<button on:click={clearTypes} class="btn hover-fill" name="Clear">
+		<svg
+			class="w-6 h-6"
+			fill="none"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+			/>
+		</svg>
+	</button>
+	<div id="dropdown" class="dropdown float-right ml-2" aria-haspopup="true" aria-expanded="false">
+		<button id="dropdown-btn" class="btn dropdown-btn hover-fill" name="More">
 			<svg
 				class="w-6 h-6"
 				fill="none"
@@ -319,159 +357,103 @@
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					stroke-width="2"
-					d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-				/>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+					d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
 				/>
 			</svg>
 		</button>
-		<button on:click={clearTypes} class="btn hover-fill" name="Clear">
-			<svg
-				class="w-6 h-6"
-				fill="none"
-				stroke="currentColor"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
+		<div id="dropdown-content" class="dropdown-content text-left">
+			<a class="modal-btn" on:click={() => open('weatherBoostModal')} role="menuitem"
+				>Weather Boost</a
 			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-				/>
-			</svg>
-		</button>
-		<div id="dropdown" class="dropdown float-right ml-2" aria-haspopup="true" aria-expanded="false">
-			<button id="dropdown-btn" class="btn dropdown-btn hover-fill" name="More">
-				<svg
-					class="w-6 h-6"
-					fill="none"
-					stroke="currentColor"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-					/>
-				</svg>
-			</button>
-			<div id="dropdown-content" class="dropdown-content text-left">
-				<a class="modal-btn" on:click={() => open('weatherBoostModal')} role="menuitem"
-					>Weather Boost</a
-				>
-				<a class="modal-btn" on:click={() => open('searchStringModal')} role="menuitem"
-					>Search Strings</a
-				>
-				<a class="modal-btn" on:click={() => open('infoModal')} role="menuitem">App Info</a>
-				<a class="modal-btn" on:click={() => open('typeChartModal')} role="menuitem">Type Chart</a>
-				<!--  				<a role="menuitem">Coming soon...</a>
+			<a class="modal-btn" on:click={() => open('searchStringModal')} role="menuitem"
+				>Search Strings</a
+			>
+			<a class="modal-btn" on:click={() => open('infoModal')} role="menuitem">App Info</a>
+			<a class="modal-btn" on:click={() => open('typeChartModal')} role="menuitem">Type Chart</a>
+			<!--  				<a role="menuitem">Coming soon...</a>
 				<a role="menuitem">App Info / Help</a>
 				<a role="menuitem">Multipliers</a>
 				<a role="menuitem">Type Chart</a>
 				<a role="menuitem">Type Quiz</a> -->
-			</div>
 		</div>
+	</div>
 
-		<h1 class="my-2 tracking-wider">
-			<button on:click={shareApp} class="btn hover-fill" name="Share">
-				<svg
-					class="w-6 h-6"
-					fill="none"
-					stroke="currentColor"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
-					/>
-				</svg>
-			</button>
-			Pokémon Types
-		</h1>
+	<h1 class="my-2 tracking-wider">
+		<button on:click={shareApp} class="btn hover-fill" name="Share">
+			<svg
+				class="w-6 h-6"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+				/>
+			</svg>
+		</button>
+		Pokémon Types
+	</h1>
 
-		<div class="my-2">
-			<Autocomplete
-				id="search"
-				placeholder="Search for a Pokémon..."
-				options={pokemonNames}
-				maxResults={50}
-				selectOnFocus={true}
-				selectOnClick={true}
-			/>
+	<div class="my-2">
+		<Autocomplete
+			id="search"
+			placeholder="Search for a Pokémon..."
+			options={pokemonNames}
+			maxResults={50}
+			selectOnFocus={true}
+			selectOnClick={true}
+		/>
+	</div>
+
+	{#if $settings.weatherBoost.weatherBoostEnabled}
+		<div id="weather-boost-select" class="relative inline-block ml-2">
+			<select id="weather-boost-options" class="relative inline-block">
+				{#each ['None', 'Sunny/Clear', 'Rainy', 'Partly Cloudy', 'Windy', 'Snow', 'Fog'] as val}
+					<option value={val.toString()}>{val}</option>
+				{/each}
+			</select>
 		</div>
+	{/if}
 
+	<p>
 		{#if $settings.weatherBoost.weatherBoostEnabled}
-			<div id="weather-boost-select" class="relative inline-block ml-2">
-				<select id="weather-boost-options" class="relative inline-block">
-					{#each ['None', 'Sunny/Clear', 'Rainy', 'Partly Cloudy', 'Windy', 'Snow', 'Fog'] as val}
-						<option value={val.toString()}>{val}</option>
-					{/each}
-				</select>
-			</div>
+			<svg
+				id="weather-boost-check"
+				class="w-4 h-4 hidden"
+				fill="none"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+			</svg>
+			<small id="weather-boost-text" class="hidden" />
 		{/if}
+	</p>
 
-		<p>
-			{#if $settings.weatherBoost.weatherBoostEnabled}
-				<svg
-					id="weather-boost-check"
-					class="w-4 h-4 hidden"
-					fill="none"
-					stroke="currentColor"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M5 13l4 4L19 7"
-					/>
-				</svg>
-				<small id="weather-boost-text" class="hidden" />
-			{/if}
-		</p>
+	<div id="type-btns" class="grid grid-cols-3 md:grid-cols-6 xl:grid-cols-9 my-2">
+		{#each TYPE_DATA as type}
+			<button class="btn {type.name}" name={type.name} on:click={() => handleClick(type.name)}>
+				<img class="type-icon" src="/img/types/{type.name}.svg" alt={type.name} />
+				<span>{capitalize(type.name)}</span>
+			</button>
+		{/each}
+	</div>
 
-		<div id="type-btns" class="grid grid-cols-3 md:grid-cols-6 xl:grid-cols-9 my-2">
-			{#each TYPE_DATA as type}
-				<button class="btn {type.name}" name={type.name} on:click={() => handleClick(type.name)}>
-					<img class="type-icon" src="/img/types/{type.name}.svg" alt={type.name} />
-					<span>{capitalize(type.name)}</span>
-				</button>
-			{/each}
+	<div id="matchups" class="grid grid-cols-2 my-2">
+		<div>
+			<div id="type-weak" class="mr-2 md:mr-4" />
 		</div>
-
-		<div id="matchups" class="grid grid-cols-2 my-2">
-			<div>
-				<div id="type-weak" class="mr-2 md:mr-4" />
-			</div>
-			<div>
-				<div id="type-resist" class="mr-2 md:ml-4" />
-			</div>
+		<div>
+			<div id="type-resist" class="mr-2 md:ml-4" />
 		</div>
-	</section>
+	</div>
+</section>
 
-	<div id="snackbar" />
+<div id="snackbar" />
 
-	<p id="pokemon-with-type" class="mt-4" />
-
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-JDML9CJTDG"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-		gtag('config', 'G-JDML9CJTDG');
-	</script>
-</body>
+<p id="pokemon-with-type" class="mt-4" />
