a73111cfc3ac5af3e700c853930af91626455b47
Justin Golden <justingolden21@gmail.com>
2022-06-20 23:03:21 -0700

    Improvements to searchstring modal

--- src/components/modals/SearchStringModal.svelte
+++ src/components/modals/SearchStringModal.svelte
@@ -24,54 +24,26 @@
 			.forEach((elm) => elm.addEventListener('change', updateSearchString));
 
 		document.getElementById('multiplier-threshold-select').onchange = updateSearchString;
-
-		// maintain toggle states such that if any is checked, fast and charge aren't
-		// and if fast or charge are checked, any isn't
-		document.getElementById('has-any-move-toggle').addEventListener('change', () => {
-			if (document.getElementById('has-any-move-toggle').checked) {
-				document.getElementById('has-fast-move-toggle').checked = false;
-				document.getElementById('has-charge-move-toggle').checked = false;
-			}
-		});
-		document.getElementById('has-fast-move-toggle').addEventListener('change', () => {
-			if (document.getElementById('has-fast-move-toggle').checked) {
-				document.getElementById('has-any-move-toggle').checked = false;
-			}
-		});
-		document.getElementById('has-charge-move-toggle').addEventListener('change', () => {
-			if (document.getElementById('has-charge-move-toggle').checked) {
-				document.getElementById('has-any-move-toggle').checked = false;
-			}
-		});
-
-		// hide / show option if applicable
-
-		document.getElementById('super-against-toggle').addEventListener('change', () => {
-			document.getElementById('multiplier-threshold-div').style.display = document.getElementById(
-				'super-against-toggle'
-			).checked
-				? 'block'
-				: 'none';
-		});
-
-		// copy btn
-		document.getElementById('copy-searchstring-btn').onclick = () => {
-			document.getElementById('searchstring-output').select();
-			document.execCommand('copy');
-		};
 	});
+	const copySearchString = () => {
+		searchstringOutput.select();
+		document.execCommand('copy');
+	};
 
-	function updateSearchString() {
-		if ($state.types[0] == '' && $state.types[1] == '') {
-			document.getElementById('searchstring-output').value = '';
-			return;
-		}
+	$: searchstring =
+		$state.types[0] === '' && $state.types[1] === ''
+			? ''
+			: getSearchString(superAgainst, hasType, hasFast, hasCharge, hasAny);
 
-		const superAgainst = document.getElementById('super-against-toggle').checked;
-		const hasType = document.getElementById('has-type-toggle').checked;
-		const hasFast = document.getElementById('has-fast-move-toggle').checked;
-		const hasCharge = document.getElementById('has-charge-move-toggle').checked;
-		const hasAny = document.getElementById('has-any-move-toggle').checked;
+	let superAgainst = false,
+		hasType = true,
+		hasFast = true,
+		hasCharge = true,
+		hasAny = false;
+
+	let searchstringOutput;
+
+	function getSearchString(superAgainst, hasType, hasFast, hasCharge, hasAny) {
 		let str = '';
 
 		if (!superAgainst) {
@@ -133,16 +105,16 @@
 			}
 		}
 
-		document.getElementById('searchstring-output').value = str;
+		return str;
 	}
 </script>
 
 <div id="search-string-type-display" class="mb-2" />
 
-<Toggle id="super-against-toggle" checked={false} labelText="Super Effective Against" />
+<Toggle checked={superAgainst} labelText="Super Effective Against" />
 
 <br />
-<div id="multiplier-threshold-div" style="display:none">
+<div class={superAgainst ? '' : 'hidden'}>
 	<label for="multiplier-threshold-select">Minimum Effectiveness Multiplier</label>
 	<select id="multiplier-threshold-select" class="relative inline-block">
 		{#each [1.2, 1.6, 1.92, 2.56, 3.07] as val}
@@ -153,22 +125,44 @@
 
 <hr />
 
-<Toggle id="has-type-toggle" checked={false} labelText="Has type" />
+<Toggle checked={hasType} labelText="Has type" />
 
-<Toggle id="has-fast-move-toggle" checked={false} labelText="Has fast move of type" />
+<Toggle
+	checked={hasFast}
+	on:change={() => {
+		if (hasFast) hasAny = false;
+	}}
+	labelText="Has fast move of type"
+/>
 
-<Toggle id="has-charge-move-toggle" checked={false} labelText="Has charge move of type" />
+<Toggle
+	checked={hasCharge}
+	on:change={() => {
+		if (hasCharge) hasAny = false;
+	}}
+	labelText="Has charge move of type"
+/>
 
-<Toggle id="has-any-move-toggle" checked={false} labelText="Has any move of type" />
+<Toggle
+	checked={hasAny}
+	on:change={() => {
+		if (hasAny) {
+			hasFast = false;
+			hasCharge = false;
+		}
+	}}
+	labelText="Has any move of type"
+/>
 
 <textarea
-	id="searchstring-output"
+	bind:this={searchstringOutput}
+	value={searchstring}
 	class="border-2 border-gray-300 p-1 mt-4 w-full"
 	spellcheck="false"
 	rows="3"
 />
 
-<button id="copy-searchstring-btn" class="btn sm hover-fill mt-2" name="Copy">
+<button on:click={copySearchString} class="btn sm hover-fill mt-2" name="Copy">
 	<svg
 		class="w-6 h-6"
 		fill="none"
