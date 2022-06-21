f94ebd1a6b1134955b871c1ed19b72db70e3ea97
Justin Golden <justingolden21@gmail.com>
2022-06-20 23:17:14 -0700

    Use copy icon in searchstring modal

--- src/components/modals/SearchStringModal.svelte
+++ src/components/modals/SearchStringModal.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Icon from '../Icon.svelte';
 	import Toggle from '../Toggle.svelte';
 	import { TYPE_NAMES, getMatchup } from '../../util/types';
 	import capitalize from '../../util/capitalize';
@@ -151,18 +152,5 @@
 />
 
 <button on:click={copySearchString} class="btn sm hover-fill mt-2" name="Copy">
-	<svg
-		class="w-6 h-6"
-		fill="none"
-		stroke="currentColor"
-		viewBox="0 0 24 24"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<path
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			stroke-width="2"
-			d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-		/>
-	</svg>
+	<Icon name="copy" class="w-6 h-6" />
 </button>
